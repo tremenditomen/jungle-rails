b28dbc0dc475b6390b2f1e403231478719f10e66
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-12 21:23:18 -0700

    implemented Feature Spec: Add to Cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the number next to My Cart update" do
+    # ACT
+    visit '/'
+    # save_screenshot
+    first('article.product').find_button('Add').click
+
+
+    # DEBUG / VERIFY
+    expect(page).to have_content 'My Cart (1)'
+    # save_screenshot
+  end
+end
